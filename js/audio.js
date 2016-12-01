7c50eb3a2b3638c509aafe68f26511cab84645d5
iamropo <iamropo@gmail.com>
2016-12-01 16:58:00 +0530

    dotted border for tracks

--- js/audio.js
+++ js/audio.js
@@ -67,7 +67,7 @@ function seekToBegining (trackNumber) {
   musicTracks[trackNumber - 1].currentTime = 0
   musicTracks[trackNumber - 1].parentElement.style.color = '#e0e0e0'
   musicTracks[trackNumber - 1].parentElement.style.fontWeight = 'normal'
-  musicTracks[trackNumber - 1].parentElement.style.borderTop = '2px solid #e0e0e0'
+  musicTracks[trackNumber - 1].parentElement.style.borderTop = '1px dotted #9e9e9e'
 }
 
 function previousTrack () {
